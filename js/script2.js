97738a4cd2b8458667775d7f1dbd40edb8a34193
james <pajussanai@gmail.com>
2020-01-06 17:32:20 +0900

    20200106

---
+++ js/script2.js
@@ -0,0 +1,8 @@
+/* burger menu */
+
+let burger = document.getElementById("burger-button");
+
+burger.addEventListener("click", (e) => {e.preventDefault();
+  document.body.classList.toggle("open");
+    burger.classList.toggle("open");
+});
